1be49a23874902bfdc506d11ae18b793392cbbbb
a22braisdr <braisdiaz97@gmail.com>
2023-11-08 14:10:26 +0100

    Commit inicial

--- models/informacion.py
+++ models/informacion.py
@@ -9,7 +9,9 @@ class informacion(models.Model):
 
     name = fields.Char(required=True, size=20, string="Titulo:")
     descripcion = fields.Text(string="A descripción:")
+    sexo = fields.Selection([('Home', 'Home'), ('Muller', 'Muller'), ('Outro', 'Outro')], string="Sexo")
     alto_en_cms = fields.Integer(string="Alto en centimetros:")
     longo_en_cms = fields.Integer(string="Longo en centimetros:")
     ancho_en_cms = fields.Integer(string="Ancho en centimetros:")
     peso = fields.Float(digits=(6, 2), default=2.7, string="Peso en kg:")
+    autorizado = fields.Boolean(default=False, string="¿Autorizado?")
