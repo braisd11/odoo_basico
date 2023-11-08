948e231b93eb93750f5f947e353c540f91bb6a0e
a22braisdr <braisdiaz97@gmail.com>
2023-11-08 13:59:01 +0100

    Commit inicial

---
+++ models/informacion.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class informacion(models.Model):
+    _name = 'odoo_basico.informacion'
+    _description = 'odoo_basico.informacion'
+
+    name = fields.Char(required=True, size=20, string="Titulo:")
+    descripcion = fields.Text(string="A descripción:")
+    alto_en_cms = fields.Integer(string="Alto en centimetros:")
+    longo_en_cms = fields.Integer(string="Longo en centimetros:")
+    ancho_en_cms = fields.Integer(string="Ancho en centimetros:")
+    peso = fields.Float(digits=(6, 2), default=2.7, string="Peso en kg:")
